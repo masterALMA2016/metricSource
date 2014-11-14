be554adc83e729f1a30ee3e0943decbafae8a409
maximepvrt <maximepvrt@gmail.com>
2014-11-14 14:34:02 +0100

    first

---
+++ src/main/java/org/master/alma/metricsource/MethodMetric.java
@@ -0,0 +1,18 @@
+package org.master.alma.metricsource;
+
+import spoon.reflect.factory.Factory;
+
+/**
+ * Created by Maxime on 14/11/14.
+ */
+public class MethodMetric {
+
+    private Factory factory;
+
+    public MethodMetric(Factory factory) {
+        this.factory = factory;
+    }
+
+
+
+}
