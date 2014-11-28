3f5c9afa6904fe6889cef5d15ac8cf904ff197a8
obloodo <davidperrai@gmail.com>
2014-11-28 16:46:26 +0100

    depthInheritenceTree metric d'ajouter

---
+++ src/main/java/org/master/alma/metricsource/DepthInheritenceTreeByMethodeMetric.java
@@ -0,0 +1,16 @@
+package org.master.alma.metricsource;
+import spoon.reflect.declaration.CtSimpleType;
+import spoon.reflect.factory.Factory;
+/**
+ * Created by david on 28/11/14.
+ */
+public class DepthInheritenceTreeByMethodeMetric extends Metric<Double> {
+
+    public DepthInheritenceTreeByMethodeMetric(Factory factory){}
+    @Override
+    public Double execute() {
+
+
+        return null;
+    }
+}
---
+++ src/test/java/org/master/alma/metricsource/DepthInheritenceTreeByMethodeMetricTest.java
@@ -0,0 +1,7 @@
+package org.master.alma.metricsource;
+
+/**
+ * Created by david on 28/11/14.
+ */
+public class DepthInheritenceTreeByMethodeMetricTest {
+}
