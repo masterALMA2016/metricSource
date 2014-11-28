6a13c214bbbe72e57ae79dbd2ea62837d454c202
obloodo <davidperrai@gmail.com>
2014-11-28 18:48:23 +0100

    testes unitaire pour la profondeur des classes ajoutés

--- src/main/java/org/master/alma/metricsource/DepthInheritenceTreeByMethodeMetric.java
+++ src/main/java/org/master/alma/metricsource/DepthInheritenceTreeByMethodeMetric.java
@@ -1,16 +1,36 @@
 package org.master.alma.metricsource;
 import spoon.reflect.declaration.CtSimpleType;
 import spoon.reflect.factory.Factory;
+
+
+import java.util.HashMap;
+
 /**
  * Created by david on 28/11/14.
  */
-public class DepthInheritenceTreeByMethodeMetric extends Metric<Double> {
+public class DepthInheritenceTreeByMethodeMetric extends Metric< HashMap<String,Integer>> {
 
-    public DepthInheritenceTreeByMethodeMetric(Factory factory){}
+    public DepthInheritenceTreeByMethodeMetric(Factory factory){ super(factory);}
+
     @Override
-    public Double execute() {
+    public HashMap<String,Integer> execute() {
+        HashMap<String,Integer>depthRes= new HashMap<>();
+           Class parent;
 
+        for(CtSimpleType s: factory.Class().getAll()){
+                int profondeur=0;
+                parent= s.getActualClass().getSuperclass();
 
-        return null;
+            while( parent != null && parent != java.lang.Object.class){
+
+                profondeur +=1;
+                parent = parent.getSuperclass();
+            }
+
+            depthRes.put(s.getQualifiedName(),profondeur);
+            System.out.println(s.getQualifiedName()+" de profondeur :"+profondeur);
+
+        }
+        return depthRes;
     }
 }
--- src/test/java/org/master/alma/metricsource/DepthInheritenceTreeByMethodeMetricTest.java
+++ src/test/java/org/master/alma/metricsource/DepthInheritenceTreeByMethodeMetricTest.java
@@ -1,7 +1,35 @@
 package org.master.alma.metricsource;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by david on 28/11/14.
  */
-public class DepthInheritenceTreeByMethodeMetricTest {
+public class DepthInheritenceTreeByMethodeMetricTest extends AbstractTest {
+       private DepthInheritenceTreeByMethodeMetric dit;
+
+    @Test
+    public void testExecute() throws Exception {
+            dit = new DepthInheritenceTreeByMethodeMetric(factory);
+
+            Map<String, Integer> result = dit.execute();
+
+            Map<String, Integer> expected = new HashMap<>();
+            expected.put("test.FakeAbstract", 0);
+            expected.put("test.FakeImpl", 0);
+            expected.put("test.FakeBean", 0);
+            expected.put("test.FakeService", 0);
+            expected.put("test.FakeImpl", 1);
+            expected.put("test.FakeEnum", 1);
+            expected.put("test.Fake",0);
+            expected.put("test.plop.Plop", 0);
+            expected.put("test.plop.PlopPlop", 0);
+
+
+            Assert.assertEquals("depth in tree ", expected, result);
+    }
 }
